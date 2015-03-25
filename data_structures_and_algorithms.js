cc15190d79301ae9702fa656d78b44f8865df6c7
An.nA <anna.git3@gmail.com>
2015-03-24 17:29:46 -0700

    array methods and definitions

--- data_structures_and_algorithms.js
+++ data_structures_and_algorithms.js
@@ -273,6 +273,7 @@ function printMatrix(myMatrix) {
 	}
 }
 
+printMatrix(averageTemp);
 
 // multidimensional arrays
 
@@ -289,6 +290,16 @@ for (var i = 0; i < 3; i++) {
 	}
 }
 
-printMatrix(averageTemp);
+
+// to output the matrix, we can use the following code
+
+for (var i=0; i < matrix3x3x3.length; i++) {
+	for (var j=0; j < matrix3x3x3[i].length; j++) {
+		for (var z=0; z < matrix3x3x3[i][j].length; z++) {
+			console.log(matrix3x3x3[i][j][z]);
+		}
+	}
+}
+
 
 
